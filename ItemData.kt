36678f219c958bb8dd92ae5886a1b54e03c5e170
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-07-16 23:27:36 -0600

    ULTIMOS CAMBIOS
    Clase java y kotlin

--- ItemData.kt
+++ ItemData.kt
@@ -1,2 +1,19 @@
-class ItemData {
+class ItemData(originalValue: Any) {
+    private val originalValue: Any = originalValue
+
+    fun getType(): String? {
+        return when (originalValue) {
+            is String -> "caden"
+            is Int -> "entero"
+            is Boolean -> "booleano"
+            else -> null
+        }
+    }
+
+    fun getInfo(): String {
+        return when (originalValue) {
+            is String -> "L${originalValue.length}" // largo del string
+            else -> ""
+        }
+    }
 }
--- ItemDataJ.java
+++ ItemDataJ.java
@@ -5,4 +5,47 @@ public class ItemDataJ{
                 this.originalValue = originalValue;
         }
 
+        public String getType(){
+                if (originalValue instanceof String) {
+                        return "caden";
+                } else if (originalValue instanceof Integer) {
+                        return "entero";
+                } else if (originalValue instanceof Boolean) {
+                        return "booleano";
+                } else {
+                        return null;
+
+                }
         }
+
+        public String getInfo() {
+                if (originalValue instanceof String) {
+                        String stringValue = (String) originalValue;
+                        return "L" + stringValue.length();
+                } else if (originalValue instanceof Integer) {
+                        int intValue = (Integer) originalValue;
+                        if (intValue % 10 == 0) {
+                                return "M10";
+                        } else if (intValue % 5 == 0) {
+                                return "M5";
+                        } else if (intValue % 2 == 0) {
+                                return "M2";
+                        } else {
+                                return null;
+                        }
+                } else if (originalValue instanceof Boolean) {
+                        boolean boolValue = (Boolean) originalValue;
+                        if (boolValue) {
+                                return "true";
+                        } else {
+                                return "False";
+                        }
+                } else {
+                        return null;
+                }
+        }
+
+
+
+
+}
