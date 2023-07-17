2477ad56b64eaefd580a998823e0b3fdd6a16ff7
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2023-07-16 22:41:43 -0600

    primeros cambios
    primeros cambios

---
+++ ItemDataJ.java
@@ -0,0 +1,8 @@
+public class ItemDataJ{
+        private Object originalValue;
+
+        public ItemDataJ(Object originalValue){
+                this.originalValue = originalValue;
+        }
+
+        }
